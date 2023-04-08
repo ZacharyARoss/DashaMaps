c0fc8e689af860b8b9f05a0034f8710380235a1c
Zachary <zachary@zipcoders-mbp-2.mynetworksettings.com>
2023-04-07 20:13:46 -0400

    made a test

--- src/test/java/com/github/zipcodewilmington/DashaMapTest.java
+++ src/test/java/com/github/zipcodewilmington/DashaMapTest.java
@@ -1,9 +1,48 @@
 package com.github.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * @author xtofer
  * @version 1.0.0
  * @date 10/21/19 9:05 AM
  */
-public class DashaMapTest {
+public class DashaMap1Test {
+    DashaMap map;
+
+    @Before
+    public void before(){
+        map = new DashaMap();
+    }
+    @Test
+    public void testSet(){
+        String key =  "key";
+        String value = "value";
+        long expected = 1;
+
+        map.set(key, value);
+        long actual = map.bucketsize("k");
+
+        Assert.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void testDelete(){
+
+    }
+    @Test
+    public void testGet(){
+
+    }
+    @Test
+    public void testIsEmpty(){
+
+    }
+    @Test
+    public void testSize(){
+
+    }
+
 }
